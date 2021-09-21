ca79d65f69ba8b280f06bbf976ef331ccdb882c1
Nacho Duarte <nacho.duarte.97@gmail.com>
2021-09-20 23:27:52 -0300

    ajuste de porcentajes logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,10 @@ function NavBar() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid navbarDisplay">
                 <a className="logoDisplay" href="#">
-                    <img src={logo} width="100%"></img>
+                    <img src={logo} width="80%"></img>
                 </a>
                 <div>
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav menuDisplay">
                         <li className="nav-button">
                             <a className="nav-a fontReg"  href="#">Inicio</a>
                         </li>
